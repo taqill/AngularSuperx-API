966b4a307d23c3f7df07658b96f53c1abf80c194
taqil <phonrat333@gmail.com>
2024-08-22 14:08:33 +0700

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    dotnet_api_url: 'https://dotnetstockrender.onrender.com/api/',
-    dotnet_api_url_image: 'https://dotnetstockrender.onrender.com/api/',
+    dotnet_api_url: 'https://dotnetstockapi-dalh.onrender.com/api/',
+    dotnet_api_url_image: 'https://dotnetstockapi-dalh.onrender.com/api/',
     base_url: '',
     token: '',
     version: '1.0',
